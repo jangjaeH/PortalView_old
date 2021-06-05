f2dec0d4ca2dd5d4bc0b06fd642a75bc2e8662ae
jangjaeH <38302881+jangjaeH@users.noreply.github.com>
2021-06-05 16:07:39 +0900

    :rocket: update: main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'vuetify/dist/vuetify.min.css'
-import Vuetify from './plugins/vuetify'
+import vuetify from './plugins/vuetify'
 Vue.config.productionTip = false
 
-Vue.use(Vuetify);
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
-
